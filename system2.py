fd8c68d43d436fd05041465c2c26be9bc5c5c577
H. Kamran <hkamran80@users.noreply.github.com>
2018-03-17 12:34:58 -0700

    Cleanup the module's code

--- system2.py
+++ system2.py
@@ -12,10 +12,12 @@ except ImportError:
 	print("pip3 is for Python 3")
 
 username = pwd.getpwuid(os.getuid()).pw_name
+
 if platform.system() == "Darwin" or "darwin":
 	operating_system = []
 	operating_system.append("macOS")
 	operating_system.append(platform.mac_ver()[0])
+	
 ip_address = []
 ip_address.append([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
 ip_address.append(requests.get('https://api.ipify.org').text.encode("ascii"))

49f48694dfe88269ab153cda505924750c85f2b6
H. Kamran <hkamran@unisontech.org>
2018-06-28 12:22:25 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='systemx',
+      version='1.0',
+      description='The missing toolkit for Python. Includes NetTools',
+      url='http://github.com/hkamran80/systemx',
+      author='H. Kamran',
+      author_email='hkamran@unisontech.org',
+      license='GPLv3+',
+      packages=['systemx'],
+      install_requires=[],
+      zip_safe=False)
